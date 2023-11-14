442dd26776b9f02867b3cb44a84a5d83bbc3c452
cdmn137 <35605272+cdmn137@users.noreply.github.com>
2023-11-14 12:45:53 -0400

    oac.py

--- oac.py
+++ oac.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-st.set_page_config(page_title="Dashboard - Comunicaciones SINCO")
+st.set_page_config(page_title="Dashboard - Comunicaciones SINCO", page_icon="📊")
 st.write("""
 # Comunicaciones SINCO
 Fondo de Compensación Interterritorial\n
@@ -13,9 +13,6 @@ df = pd.read_excel("casos.xlsx")
 df["Transmisor/Nombre"] = df["Transmisor/Nombre"].astype(str)
 df =  df.loc[~df["Transmisor/Nombre"].str.contains("prueba"), ]
 
-df = df.dropna(subset=["Estatus"])
-
-#df["Estatus"] = df["Estatus"].astype(str)
 df.loc[df["Estatus"].isna(), "Estatus"] = "Ninguno"
 
 # estatus de asignacion
@@ -42,29 +39,9 @@ d2 = {
 
 df["Estatus"] = df["Estatus"].replace(d2)
 
-# -- Grafico de estatus --
-valores = df["Estatus"].value_counts()
-
-source = pd.DataFrame({"category": valores.index, "value": valores})
-
-ch = alt.Chart(source).mark_arc().encode(
-    theta="value",
-    color="category"
-)
-
-st.altair_chart(
-    (ch).interactive(),
-    use_container_width=True
-)
-
-
 list_options_estatus = []
 list_options_estatus.extend(df["Estatus"].unique().tolist())
 list_options_estatus.sort()
-
-estatus = st.sidebar.selectbox('Seleccione el estatus', list_options_estatus)
-df_process = df.loc[(df["Estatus"]==estatus), ]
-
 
 responsable = st.sidebar.selectbox('Seleccione el responsable', ['Todos', 'LUIS GERMAN RIVAS ZAMBRANO', 'CRUZ DAVID MATA NOGUERA',
        'CIRO ANTONIO RODRIGUEZ VILLANUEVA',
@@ -74,8 +51,71 @@ responsable = st.sidebar.selectbox('Seleccione el responsable', ['Todos', 'LUIS 
        'JOALI GABRIELA MORENO PINTO'])
 
 if (responsable != "Todos"):
-    df_process = df_process.loc[(df_process["Responsable/Mostrar nombre"] ==responsable), ]
-    
+    df_process = df.loc[(df["Responsable/Mostrar nombre"] ==responsable), ]
+    # -- Grafico de estatus --
+    valores = df_process["Estatus"].value_counts()
+    source = pd.DataFrame({"category": valores.index, "value": valores})
+    cd = alt.Chart(source).mark_arc().encode(
+        theta="value",
+        color="category"
+    )
+
+    st.altair_chart(
+        (cd).interactive(),
+        use_container_width=True
+    )
+else:
+    df_process = df.loc[(df["Responsable/Mostrar nombre"] ==responsable), ]
+    # -- Grafico de estatus --
+    valores = df["Estatus"].value_counts()
+    source = pd.DataFrame({"category": valores.index, "value": valores})
+    ch = alt.Chart(source).mark_arc().encode(
+        theta="value",
+        color="category"
+    )
+
+    st.altair_chart(
+        (ch).interactive(),
+        use_container_width=True
+    )
+
+    # -- Grafico de categorias --
+    st.subheader('Casos por Asunto')
+    categorias = df["Categoría/Nombre de categoría"].value_counts()
+    df_categorias = pd.DataFrame({"categoria":categorias.index, "valores":categorias})
+    bar = alt.Chart(df_categorias).mark_bar().encode(
+        x= alt.X("categoria", sort=None),
+        y="valores"
+    )
+
+    st.altair_chart(
+        (bar).interactive(),
+        use_container_width=True
+    )
+
+    t_min= df["Creado en"].min()
+
+    st.subheader('El mas urgente de atender')
+    st.dataframe(df.loc[(df["Creado en"]==t_min), ])
+
+    st.subheader('por atender rapido')
+    st.text(len(df))
+    st.dataframe(df.sort_values(by=["Creado en"]).head(10000))
+
+estatus = st.sidebar.selectbox('Seleccione el estatus', ['Todos', 'A-En Proceso', 'B-Confirmado', 'C-Vinculado', 'D-Respondido', 'E-En Espera', 'F-Sin respuesta'])
+if (estatus != "Todos"):
+    df_process = df_process.loc[(df_process["Estatus"] ==estatus), ]
+
+# Seleccion de estatus de asignaciones
+list_options = ['Todos']
+list_options.extend(df["Asignado/Estatus"].unique().tolist())
+   
+asignacion_estatus = st.sidebar.selectbox('Seleccione el estatus de asignación', list_options)
+
+if (asignacion_estatus != "Todos"):
+    df_process = df_process.loc[(df_process["Asignado/Estatus"] ==asignacion_estatus), ]
+
+# Seleccion de analista asignado
 analistas = st.sidebar.selectbox('Seleccione el analista', ['Todos', 
                                                                  'ROSARIO ELISA MATHEUS SULBARAN', 
                                                                  'NORMAN JOSE SOTO BOLIVAR', 
@@ -86,24 +126,11 @@ if (analistas != "Todos"):
     df_process = df_process.loc[(df_process["Asignado/Asignado/Nombre"] ==analistas), ]
 
 
-list_options = ['Todos']
-list_options.extend(df_process["Asignado/Estatus"].unique().tolist())
-   
-asignacion_estatus = st.sidebar.selectbox('Seleccione el estatus de asignación', list_options)
-
-if (asignacion_estatus != "Todos"):
-    df_process = df_process.loc[(df_process["Asignado/Estatus"] ==asignacion_estatus), ]
 
 # -- Grafico de categorias --
-
 st.subheader('Casos por Asunto')
-
-
-
 categorias = df_process["Categoría/Nombre de categoría"].value_counts()
-
 df_categorias = pd.DataFrame({"categoria":categorias.index, "valores":categorias})
-
 bar = alt.Chart(df_categorias).mark_bar().encode(
     x= alt.X("categoria", sort=None),
     y="valores"
@@ -115,13 +142,12 @@ st.altair_chart(
 )
 
 
-
 t_min= df_process["Creado en"].min()
-
 
 st.subheader('El mas urgente de atender')
 st.dataframe(df_process.loc[(df_process["Creado en"]==t_min), ])
 
 st.subheader('por atender rapido')
 st.text(len(df_process))
-st.dataframe(df_process.sort_values(by=["Creado en"]).head(100))
+st.dataframe(df_process.sort_values(by=["Creado en"]).head(10000))
+
